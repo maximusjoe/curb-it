4c275d57318cc9339af2affc53cb6cd1b761fdc2
Tony Trinh <tony.q.trinh@gmail.com>
2021-05-11 15:09:19 -0700

    Added some layouts to main.html, changed global styles.css.

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -1,5 +1,19 @@
-jQuery(function() {
+jQuery(function () {
+    'use strict'
     $('#make-request-button').on('click', (e) => {
-        $('#make-request-post').text('redirect/pop-up to post-successful window')
+        $('#make-request-post').toggle(0);
     })
+
+    const arrayOfItems = [
+        'HELLO', 'HI', 'THREE', 'FOUR', 'FIVE', 'MOCK DATA','RANDOM'
+    ];
+
+    const htmlNewsFeed;
+    for (let i = 0; i < arrayOfItems.length; i++) {
+        
+    }
+
+    $('.newsfeed').html(
+        '<p></p>'
+    )
 })
