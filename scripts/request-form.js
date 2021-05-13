f136e32b8295fc9f4300084bc48e336c58240239
XANDRA <tuhudohoi102@gmail.com>
2021-05-13 00:46:37 -0700

    display added items right after press add-button

--- scripts/request-form.js
+++ scripts/request-form.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
+    $("#add-button").click(function(e) {
+        e.preventDefault();
+
+        let idCount = 0;
+
+        let item_input = $("#list-input").val();
+        itemArray.push(item_input);
+
+        console.log(item_input);
+
+        $("#list-input").before('<div id="' + itemArray.length + '" class="each-item"><span class="item-name">' + item_input + '</span><button id="delete' + itemArray.length + '" type="button" class="cross btn-close"></button></div>')
+
+    })
+
+    var itemArray = new Array();
+    console.log(itemArray);
 
 });
