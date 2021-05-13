8633cf6f6c6e70004030239d2e26d7126e0be87e
XANDRA <tuhudohoi102@gmail.com>
2021-05-13 12:42:49 -0700

    re-organize the code, clean up

--- scripts/request-form.js
+++ scripts/request-form.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
         console.log(item_input);
 
-        $("#list-input").before('<div id="' + itemArray.length + '" class="each-item"><span class="item-name">' + item_input + '</span><button id="delete' + itemArray.length + '" type="button" class="cross btn-close"></button></div>')
+        $("#add-button").after('<div id="' + itemArray.length + '" class="each-item"><span class="item-name">' + item_input + '</span><button id="delete' + itemArray.length + '" type="button" class="cross btn-close"></button></div>')
 
     })
 
@@ -19,34 +19,52 @@ $(document).ready(function() {
     $('#submit-button').click(function(e) {
         e.preventDefault();
 
+        let address = $("#address-input").val();
+        let city = $("#city-input").val();
+        let width = $("#width-input").val();
+        let height = $("#width-input").val();
+        let numberOfItem = $("#number-input").val();
+        console.log(numberOfItem);
+
+
         firebase.auth().onAuthStateChanged(function(user) {
             if (user) {
-                var dateControl = document.querySelector('input[type="date"]');
-                var timeControl = document.querySelector('input[type="time"]');
+                db.collection("users").doc(user.uid)
+                    .get()
+                    .then(function(doc) {
+                        var n = doc.data().name;
 
-                console.log($("#address-input").val().toString());
-                console.log($("#list-input").val().toString());
-                console.log($("#photo-input").val().toString());
-                console.log(dateControl.value);
-                console.log(timeControl.value);
-                console.log(getDateTime().toString());
+                        db.collection("users").doc(user.uid).collection("postedRequests").add({
+                            name: n,
+                            address: address,
+                            city: city,
+                            //photo: $("#photo-input").val(),
+                            list: itemArray,
+                            width: width,
+                            height: height,
+                            numberOfItem: numberOfItem,
+                            postedDate: getDateTime()
+                        });
 
-                db.collection("users").doc(user.uid).collection("postedRequests").get().then(function(querySnapshot) {
-                    var postID = querySnapshot.size + 1;
-                    console.log(postID);
-                    db.collection("users").doc(user.uid).collection("postedRequests").doc(postID.toString()).set({
-                        address: $("#address-input").val().toString(),
-                        items: $("#list-input").val().toString(),
-                        photo: $("#photo-input").val().toString(),
-                        date: dateControl.value,
-                        time: timeControl.value,
-                        postedDate: getDateTime()
-                    });
-                });
+                        db.collection("publicRequests").add({
+                            name: n,
+                            address: address,
+                            city: city,
+                            //photo: $("#photo-input").val(),
+                            list: itemArray,
+                            width: width,
+                            height: height,
+                            numberOfItem: numberOfItem,
+                            postedDate: getDateTime()
+                        });
+
+
+                    })
 
             } else {
                 // No user is signed in.
-                console.log("ERROR: No User Signed In");
+                console.log("No User Signed In");
+                window.location.href = "login.html";
             }
         });
     });
