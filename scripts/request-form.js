8b7cb61951baa6d87fb39d57bc41bde696a89c7d
XANDRA <tuhudohoi102@gmail.com>
2021-05-13 10:42:50 -0700

    merge with Marcro's branch

--- scripts/request-form.js
+++ scripts/request-form.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
     $("#add-button").click(function(e) {
         e.preventDefault();
@@ -21,10 +20,10 @@ $(document).ready(function() {
         e.preventDefault();
 
         firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {  
+            if (user) {
                 var dateControl = document.querySelector('input[type="date"]');
                 var timeControl = document.querySelector('input[type="time"]');
-                
+
                 console.log($("#address-input").val().toString());
                 console.log($("#list-input").val().toString());
                 console.log($("#photo-input").val().toString());
@@ -32,35 +31,35 @@ $(document).ready(function() {
                 console.log(timeControl.value);
                 console.log(getDateTime().toString());
 
-                db.collection("users").doc(user.uid).collection("postedRequests").get().then(function(querySnapshot) {  
+                db.collection("users").doc(user.uid).collection("postedRequests").get().then(function(querySnapshot) {
                     var postID = querySnapshot.size + 1;
-                    console.log(postID); 
-                     db.collection("users").doc(user.uid).collection("postedRequests").doc(postID.toString()).set({
-                         address: $("#address-input").val().toString(),
-                         items: $("#list-input").val().toString(),
-                         photo: $("#photo-input").val().toString(),
-                         date: dateControl.value,
-                         time: timeControl.value,
-                         postedDate: getDateTime()
-                       });
+                    console.log(postID);
+                    db.collection("users").doc(user.uid).collection("postedRequests").doc(postID.toString()).set({
+                        address: $("#address-input").val().toString(),
+                        items: $("#list-input").val().toString(),
+                        photo: $("#photo-input").val().toString(),
+                        date: dateControl.value,
+                        time: timeControl.value,
+                        postedDate: getDateTime()
                     });
+                });
 
             } else {
-              // No user is signed in.
-              console.log("ERROR: No User Signed In");
+                // No user is signed in.
+                console.log("ERROR: No User Signed In");
             }
-          }); 
+        });
     });
 
 });
 
 function getDateTime() {
-    var currentdate = new Date(); 
-    var datetime = currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
+    var currentdate = new Date();
+    var datetime = currentdate.getDate() + "/" +
+        (currentdate.getMonth() + 1) + "/" +
+        currentdate.getFullYear() + " @ " +
+        currentdate.getHours() + ":" +
+        currentdate.getMinutes() + ":" +
+        currentdate.getSeconds();
     return datetime;
 }
