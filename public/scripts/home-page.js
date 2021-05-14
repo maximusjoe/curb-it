b2eadb44cdafcdf280b3bf0fbdb0e40372dea8b7
Tony Trinh <tony.q.trinh@gmail.com>
2021-05-14 01:28:57 -0700

    Create skeleton for about.html About Us page. Clean up code and Modify 404 page.

--- public/scripts/home-page.js
+++ public/scripts/home-page.js
@@ -6,15 +6,15 @@ jQuery(function () {
             querySnapshot.forEach((doc) => {
                 postsArray.push(doc.data());
             });
+            $('#newsfeed-container').empty()
             for (let i = 0; i < postsArray.length; i++) {
-                $('#newsfeed-container').append(`<a href="#" class="list-group-item list-group-item-action" aria-current="true">
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">${postsArray[i].ID}</h5>
-            <small>3 days ago</small>
-          </div>
-          <p class="mb-1">${postsArray[i].address}.</p>
-          <small>${postsArray[i].items}.</small>
-        </a>`)
+                if (postsArray[i].available === true) {
+                    $('#newsfeed-container')
+                        .append(`<a href="#" class="list-group-item list-group-item-action" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">${postsArray[i].ID}</h5>
+                        <small>3 days ago</small></div><p class="mb-1">${postsArray[i].address}.</p><small>${postsArray[i].items}.</small></a>`)
+                }
             }
         });
 })
