8398e021a529a8ad4fbe8281617ad3d6585bfcc3
XANDRA <tuhudohoi102@gmail.com>
2021-05-17 16:07:38 -0700

    add click listener in each request and redirect to according page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,15 +1,16 @@
-$(document).ready(function () {
+$(document).ready(function() {
     'use strict'
+
     function aFunc() {
         console.log('hi')
     }
 
     function allRequests() {
-        firebase.auth().onAuthStateChanged(function (user) {
+        firebase.auth().onAuthStateChanged(function(user) {
             db.collection("users")
                 .get()
-                .then(function (userCol) {
-                    userCol.forEach(function (eachUser) {
+                .then(function(userCol) {
+                    userCol.forEach(function(eachUser) {
                         // console.log("Username: " + eachUser.data().name);
                         userRequests(eachUser.id)
                     })
@@ -19,28 +20,37 @@ $(document).ready(function () {
 
     allRequests();
 
-    window.aFunc= aFunc;
+    window.aFunc = aFunc;
 
     function userRequests(userID) {
-        firebase.auth().onAuthStateChanged(function (user) {
+        firebase.auth().onAuthStateChanged(function(user) {
             db.collection("users").doc(userID).collection("postedRequests")
                 .get() //READ asynch
-                .then(function (reqCol) {
-                    reqCol.forEach(function (req) { //read each document in the collection
+                .then(function(reqCol) {
+                    reqCol.forEach(function(req) { //read each document in the collection
                         // console.log("Request: " + req.id);
-                        let eachPost = '<a onClick="aFunc();"><div class="each-post" id="' + req.id + '"><div class="number-of-item">' + req.data().numberOfItem + ' item(s)</div><div class="city">' + req.data().address + '</div><div class="date">Posted on ' + req.data().postedDate + '</div></div></a>'
+                        let eachPost = '<div class="each-post" id="' + req.id + '"><div class="number-of-item">' + req.data().numberOfItem + ' item(s)</div><div class="city">' + req.data().address + '</div><div class="date">Posted on ' + req.data().postedDate + '</div></div></a>'
                         if (req.data().available) {
                             $("#content").append(eachPost);
+                            redirectToInfo(req.id);
                         }
                     })
                 })
         });
     }
 
-    
-    $("#filter-button").click(function () {
+
+    $("#filter-button").click(function() {
         $("#filterContainer").toggle(250);
     });
 
-    
+    function redirectToInfo(id) {
+        document.getElementById(id)
+            .addEventListener("click", function() {
+                console.log(id + "was clicked!")
+                window.location.href = "request-info.html?id=" + id; // Redirect page when click
+            });
+    }
+
+
 });
