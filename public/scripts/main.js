6427a9ce52bb653a45481edae02d5f9ae87bd637
Maximus Joe <maximusjoe278@gmail.com>
2021-05-20 07:56:30 -0700

    created sort and filter system

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
                 .get() //READ asynch
                 .then(function(reqCol) {
                     reqCol.forEach(function(req) { //read each document in the collection
-                        let eachPost = `<div class="each-post" id="${req.id}"><div class="number-of-item">${req.data().numberOfItem} item(s)</div><div class="city"> ${req.data().city}</div><div class="date">Posted on ${req.data().postedDate}</div></div>`
+                        let eachPost = `<div class="each-post" id="${req.id}" data-items="${req.data().numberOfItem}" data-city="${req.data().city}" data-date="${req.data().postedDate}"><div class="number-of-item">${req.data().numberOfItem} item(s)</div><div class="city"> ${req.data().city}</div><div class="date">Posted on ${req.data().postedDate}</div></div>`
                         $('#spinner').hide()
                         if (req.data().available) {
                             $("#content").append(eachPost);
@@ -43,6 +43,39 @@ $(document).ready(function() {
                 window.location.href = `request-info.html?id=${id}&poster=${uid}` // Redirect page when click
             });
     }
+
+    $("#apply").click(function() {
+        if (document.getElementById("all-select").value == "sortLowToHigh"){
+            $("#content .each-post")
+                .sort(function (a, b) {
+                    return $(a).data("items") - $(b).data("items");
+                })
+                .appendTo("#content");
+        }
+        if (document.getElementById("all-select").value == "sortHighToLow"){
+            $("#content .each-post")
+                .sort(function (a, b) {
+                    return $(b).data("items") - $(a).data("items");
+                })
+                .appendTo("#content");
+        }
+        if (document.getElementById("all-select").value == "sortCity"){
+           
+            jQuery.fn.sortDivs = function sortDivs() {
+                $("> div", this[0]).sort(dec_sort).appendTo(this[0]);
+                function dec_sort(a, b){ return ($(b).data("city")) < ($(a).data("city")) ? 1 : -1; }
+            }
+            $("#content").sortDivs();
+        }
+        if (document.getElementById("all-select").value == "sortDate"){
+            jQuery.fn.sortDivs = function sortDivs() {
+                $("> div", this[0]).sort(dec_sort).appendTo(this[0]);
+                function dec_sort(b, a){ return ($(b).data("date")) < ($(a).data("date")) ? 1 : -1; }
+            }
+            $("#content").sortDivs();
+        }
+        
+    });
 
 
 });
